cf548fd3cb82050490b8eb1f2a89ea0b9a64dfec
Dev.shaon <developershaon11@gmai.com>
2025-03-30 00:06:00 +0600

    intial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const video1 = document.getElementById('projectvideo1');
+const video2 = document.getElementById('projectvideo2');
+const video3 = document.getElementById('projectvideo3');
+const hoversing = document.querySelector(".hoversing")
+
+
+const videolist = [video1, video2, video3];
+
+
+videolist.forEach(function(video){
+    video.addEventListener('mouseover', function(){
+        video.play();
+        hoversing.classList.add("active");
+    })
+    video.addEventListener('mouseout', function() {
+        video.pause();
+        hoversing.classList.remove("active");
+    })
+})
+
+
+
+window.addEventListener("scroll", () => {
+    let scrollY = window.scrollY; // Get scroll position
+    let blurValue = Math.min(scrollY / 70, 40); // Limit blur to 40px
+
+    document.querySelector(".autoBlur").style.filter = `blur(${blurValue}px)`;
+});
