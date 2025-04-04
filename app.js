70702445285bee750c34446f79a1197a58a454de
Dev.shaon <developershaon11@gmai.com>
2025-04-04 16:15:07 +0600

    preloader duiretion 60

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Animation function for progress
   function animateProgress() {
-    const duration = 20; // Total loading time in seconds
+    const duration = 60; // Total loading time in seconds
 
     gsap.to(
       {},
